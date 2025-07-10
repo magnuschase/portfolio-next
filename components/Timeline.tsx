72b2369dabfabc17a85b57b51817a634c4706ed6
Jakub Kapała <jakub@kapala.xyz>
2025-07-10 11:12:07 +0200

    feat: reverse timeline display

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -29,7 +29,7 @@ const Timeline: React.FC<TimelinePayload> = ({data}) => {
 					whileInView={{ opacity: 1, scale: 1 }}
 					viewport={{ once: true }}
 					transition={{ duration: 0.7, type: 'spring', delay: 0.3 }} className="grid grid-cols-2">
-					{data.map((element, index) => {
+					{data.reverse().map((element, index) => {
 						if (index % 2 == 0) return (
 							<React.Fragment key={element.abbr}>
 								<div className="flex justify-center items-center relative pl-12" key={element.abbr}>
@@ -47,7 +47,8 @@ const Timeline: React.FC<TimelinePayload> = ({data}) => {
 								<div className="h-32 border-l-4 border-kapala-700"></div>
 							</React.Fragment>
 						)
-						else return (
+						
+						return (
 							<React.Fragment key={element.abbr}>
 								<div className="h-32 border-kapala-700"></div>
 								<div className="flex justify-center items-center relative border-l-4 border-kapala-700 pr-12" key={element.abbr}>
